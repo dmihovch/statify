aa93bba44156f7678b08fa217735944cc122f69f
dmihovch <dmihovch@gmail.com>
2025-03-20 19:27:56 -0400

    can now collect and format spotify data

--- data-collection/collect_data.py
+++ data-collection/collect_data.py
@@ -5,40 +5,70 @@ import os
 
 
 
-load_dotenv()
 
-client_id = os.getenv("SPOTIFY_CLIENT_ID")
-client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+def collect_data_and_format():
+    load_dotenv()
 
-
-scope = "user-library-read user-library-modify playlist-modify-private playlist-modify-public user-top-read user-read-playback-state user-modify-playback-state app-remote-control user-read-private streaming"
-#gives full control, might change later
+    client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
 
-oauth = (
-    client_id,
-    client_secret,
-    redirect_uri,
-    scope
-)
+    scope = "user-library-read user-library-modify playlist-modify-private playlist-modify-public user-top-read user-read-playback-state user-modify-playback-state app-remote-control user-read-private streaming"
+    #gives full control, might change later
 
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope=scope))
-
-top_tracks = sp.current_user_top_tracks(time_range="short_term")
-top_artists = sp.current_user_top_artists(limit=1, time_range="short_term")
 
 
-print(top_artists['items'][0]['genres'])
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope=scope))
+
+    top_tracks = sp.current_user_top_tracks(time_range="short_term")
+    top_artists = sp.current_user_top_artists(time_range="short_term")
+
+    tt = top_tracks['items']
+    ta = top_artists['items']
 
 
-'''
-for idx, track in enumerate(top_tracks['items']):
-    print(f"{idx+1}. {track['name']} by {track['artists'][0]['name']}")
+    json_obj = {
+        "top_tracks" : [],
+        "top_artists": []
+    }
+
+
+
+    for track in tt:
+        
+        track_entry = {
+            "track_name" : track['name'],
+            "artists": [],
+            "popularity": track['popularity'],
+            "image": track['album']['images'][0]['url']
+        }
+        
+        artists_list = []
+        
+        for artist in track['artists']:
+            artists_list.append(artist['name'])
+        
+        track_entry["artists"] = artists_list
+        
+        json_obj["top_tracks"].append(track_entry)
+
+
+    for artist in ta:
+        artist_entry = {
+            "name" : artist['name'],
+            "popularity": artist['popularity'],
+            "followers": artist['followers']['total'],
+            "genres": artist['genres'],
+            "image": artist['images'][0]['url']
+        }
+        json_obj['top_artists'].append(artist_entry)
     
-    
-print("TOP ARTISTS")
-for id, artist in enumerate(top_artists['items']):
-    print(f"{id+1}.",artist['name'])
-'''
+    return json_obj
+
+
+
+
+
