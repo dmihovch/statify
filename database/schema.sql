0ae6302cd84b0cb7bf98706bcb72e7c38cfe01ad
dmihovch <dmihovch@gmail.com>
2025-04-12 23:44:56 -0400

    im sqlinggggg

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,19 @@
-CREATE TABLE stats (
+CREATE TABLE IF NOT EXISTS users (
+	clerk_id TEXT PRIMARY KEY,
+	spotify_id TEXT,
+	access_token TEXT NOT NULL,
+	refresh_token TEXT NOT NULL,
+	token_expiry INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS stats (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	json_data TEXT,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
-)
+	clerk_id TEXT NOT NULL,
+	fetched_at INTEGER NOT NULL,
+	data TEXT NOT NULL,
+	FOREIGN KEY (clerk_id) REFERENCES users(clerk_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_stats_user_date
+ON stats(clerk_id, fetched_at);
